09b67f1c5601ca185894f758cd3be8a576191bd7
Dang Dong Minh <dangdongminh123@gmail.com>
2016-12-14 17:13:37 +0700

    Initalize

---
+++ src/main.js
@@ -0,0 +1,20 @@
+/*class User{
+  constructor(username, email, password){
+      this.username = username;
+      this.email = email;
+      this.password = password;
+  }
+
+  static countUsers(){
+    console.log('There are 50 users');
+  }
+
+  register(){
+    console.log(this.username+' is now registered');
+  }
+}
+
+let minh = new User('minh', 'minh@gmail.com', '12345');
+minh.register();
+User.countUsers();
+*/
